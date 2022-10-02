0a8e913b8ea3ec617d39f93383a8c691056753c2
chrispus1000 <minrevac@gmail.com>
2022-10-02 03:04:10 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,19 +1,26 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record_warning
 
   def index
     toys = Toy.all
     render json: toys
   end
 
+  def show 
+    toy = Toy.find_by(id: params[:id])
+    render json: toy, status: :ok
+  end 
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
@@ -27,5 +34,9 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
+
+  def render_invalid_record_warning(invalid)
+    render json: {errors: invalid.record.errors}
+  end 
 
 end
